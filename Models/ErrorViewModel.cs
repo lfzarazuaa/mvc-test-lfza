a924e250ab9945f493407c5ca041a86245198bef
LuisZ <lfzarazuaa1400@outlook.com>
2022-04-16 11:20:23 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mvc_test_lfza.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
